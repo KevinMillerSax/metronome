64d2117c75085a7a0fdb03ab9e0ea8fa742ecea3
System Administrator <root@kevins-mbp.home>
2019-08-12 15:49:06 -0700

    Updates

---
+++ precache-manifest.4c8844b38f99fb2e1aa90795189f6e91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c00100e22e8a671a580a916d5dc8f5",
+    "url": "/KevinMillerSax/metronome/index.html"
+  },
+  {
+    "revision": "bb7bd839465b37c2089a",
+    "url": "/KevinMillerSax/metronome/static/css/main.5c597e90.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/KevinMillerSax/metronome/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "bb7bd839465b37c2089a",
+    "url": "/KevinMillerSax/metronome/static/js/main.49a71681.chunk.js"
+  },
+  {
+    "revision": "cf7ac36d971fc02601c0",
+    "url": "/KevinMillerSax/metronome/static/js/runtime~main.fb8926a2.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/KevinMillerSax/metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/KevinMillerSax/metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
